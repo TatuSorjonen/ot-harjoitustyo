6562ba766a59339fdd0b4896390a2ad3bf1dbb6d
Tatu Sorjonen <tatu.sorjonen@gmail.com>
2022-04-12 22:01:29 +0300

    Viikko 4 commitus

--- src/grid.py
+++ src/grid.py
@@ -1,36 +1,51 @@
 import pygame
 
+
 class Grid:
-    
-    def __init__(self, grid_size, num_squares, num_for_win):    
-        self.BLACK = (0, 0, 0)
-        self.WHITE = (200, 200, 200)
+
+    def __init__(self, grid_size, num_squares, num_for_win):
+        self.black_grid = (0, 0, 0)
+        self.white_grid = (200, 200, 200)
         self.grid_size = grid_size
         self.num_squares = num_squares
         self.num_for_win = num_for_win
         self.block_size = int(self.grid_size/self.num_squares)
-              
+        self.whose_turn = 1
+        self.how_many_characters = 0
+        #Images
+        self.x_image = pygame.transform.scale(pygame.image.load("src/Images/x-icon-white-20.jpg"),(self.block_size, self.block_size))
+        self.o_image = pygame.transform.scale(pygame.image.load("src/Images/png_letter_o_51833.png"), (self.block_size, self.block_size))
         pygame.init()
         self.screen = pygame.display.set_mode([self.grid_size, self.grid_size])
-        self.screen.fill((self.WHITE))
-        
+        self.screen.fill((self.white_grid))
         self.running = True
 
     def run(self):
         while self.running:
             self.draw_grid()
-
             for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.draw_xo(event)
                 if event.type == pygame.QUIT:
                     self.running = False
-
             pygame.display.flip()
-
         pygame.quit()
-        
-    def draw_grid(self):
-        for x in range(0, self.grid_size, self.block_size):
-            for y in range(0, self.grid_size, self.block_size):
-                rect = pygame.Rect(x, y, self.block_size, self.block_size)
-                pygame.draw.rect(self.screen, self.BLACK, rect, 1)
 
+    def draw_xo(self, event):
+        x_coordinate = event.pos[0]-self.x_image.get_width()/2
+        y_coordinate = event.pos[1]-self.x_image.get_width()/2
+        if self.whose_turn == 1 and self.num_squares*self.num_squares > self.how_many_characters:
+            self.screen.blit(self.x_image, (x_coordinate, y_coordinate))
+            self.whose_turn = 2
+            self.how_many_characters = self.how_many_characters + 1
+        elif self.whose_turn == 2 and self.num_squares*self.num_squares > self.how_many_characters:
+            self.screen.blit(self.o_image, (x_coordinate, y_coordinate))
+            self.whose_turn = 1
+            self.how_many_characters = self.how_many_characters + 1
+          
+
+    def draw_grid(self):
+        for x_int in range(0, self.grid_size, self.block_size):
+            for y_int in range(0, self.grid_size, self.block_size):
+                rect = pygame.Rect(x_int, y_int, self.block_size, self.block_size)
+                pygame.draw.rect(self.screen, self.black_grid, rect, 1)
--- src/tests/grid_test.py
+++ src/tests/grid_test.py
@@ -1,15 +1,19 @@
 import unittest
 from grid import Grid
 
+
 class TestGrid(unittest.TestCase):
     def setUp(self):
-        self.grid = Grid(1000, 10, 4)
-        
-    def test_grid_run(self):
-        self.grid.run()
-        
-    def test_constructor_is_working(self):
-        self.assertTrue(self.grid.block_size >= 50, "Too many squares") 
-        self.assertTrue(self.grid.block_size < self.grid.grid_size/self.grid.num_for_win, "Too little squares")
-        
+        self.grid = Grid(1000, 5, 4)
 
+    #def test_grid_run(self):
+        #self.grid.run()
+
+    def test_constructor_is_working(self):
+        self.assertTrue(self.grid.block_size >= 50, "Too many squares")
+        self.assertTrue(self.grid.block_size < self.grid.grid_size /
+                        self.grid.num_for_win, "Too little squares")
+                        
+    def test_how_many_characters_cant_go_higher_than_num_squares(self):
+        self.grid.run()
+        self.assertTrue(self.grid.how_many_characters <= (self.grid.num_squares * self.grid.num_squares))
