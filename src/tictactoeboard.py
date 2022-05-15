e358c8594964fbc08db71455b6c0fb18559f15a5
Tatu Sorjonen <tatu.sorjonen@gmail.com>
2022-05-15 15:11:03 +0300

    tictactoeboard

--- src/tictactoeboard.py
+++ src/tictactoeboard.py
@@ -10,25 +10,27 @@ class Result(Enum):
     SECOND_WIN = 4
 
 class TicTacToeBoard():
-    """Class that manages the properties of a tictactoe game. Inherits the class Board
+    """Class that manages the properties of a tictactoe game data structure
 
     Attributes:
-        Same as class Board and also
-        board: set the board full '-' signs
-        whose_turn: whose turn is it
-        winner: Who wins at end
+        num_squares: How many num squares
+        board: Two dimensional array ('-' = free square, 'x' = player 1, 'o' = player 2)
+        result: Game situation
+        player1: player 1 name
+        player2: player 2 name
+        whose_turn: whose turn is it (1 or 2)
     """
 
     def __init__(self, num_squares, player1, player2):
         """Class contsructor which set all attributes at starting position
 
         Args:
-            num_squares: how big is tictactoe board. Row and column
-
+            num_squares: How big is tictactoe board. Numbers of rows and columns
+            player1: Player 1 name
+            player2: Player 2 name
         """
 
         self.num_squares = num_squares
-        self.board = [[]]
         self.result = Result.ONGOING
         self.player1 = player1
         self.player2 = player2
@@ -42,7 +44,6 @@ class TicTacToeBoard():
         Args:
             x_square: What x square is it
             y_square: What y square is it
-
         """
 
         if not self.is_taken(y_square, x_square):
@@ -58,7 +59,6 @@ class TicTacToeBoard():
         Args:
             x_square: What x square is it
             y_square: What y square is it
-
         """
 
         if not self.is_taken(y_square, x_square):
@@ -69,8 +69,8 @@ class TicTacToeBoard():
 
     def check_situation(self):
         """Goes through the entire board and check if there are x or o characters on the board.
-        if so then call the check_winner function. And after all places are check, calls check_draw
-        funcion
+        If so then call the check_winner function.
+        And after all places are checked, calls check_draw funcion
         """
 
         for i in range(0, self.num_squares):
@@ -83,7 +83,12 @@ class TicTacToeBoard():
         self.check_draw()
 
     def check_winner(self, i, j):
-        """Check if someone wins or not. First check row, then col. After that diagonals
+        """Check if someone wins or not (4 in a row).
+        First check row, then col. After that diagonals
+
+        Args:
+            i: y coordinate on board
+            j: x coordinate on board
         """
 
         if j + 3 < self.num_squares:
@@ -106,7 +111,6 @@ class TicTacToeBoard():
                                 == self.board[i-3][j+3]:
                 self.set_winner(self.board[i][j])
 
-    # Sets winner for game
     def set_winner(self, winner):
         """Set winner for game after checking if winner is x or not
         Args:
@@ -118,7 +122,6 @@ class TicTacToeBoard():
         else:
             self.result = Result.SECOND_WIN
 
-    # Check if square is taken and returns True or False
     def is_taken(self, y_square, x_square):
         """Check if square is taken
 
@@ -127,7 +130,7 @@ class TicTacToeBoard():
             y_square: What y square is it
 
         Returns:
-            True, if square is taken so it is not '-'
+            True, if square is taken, otherwise returns False
         """
 
         return self.board[y_square][x_square] != '-'
@@ -138,7 +141,3 @@ class TicTacToeBoard():
 
         if not any('-' in x for x in self.board) and self.result == Result.ONGOING:
             self.result = Result.DRAW
-
-    def print_board(self):
-        for i in self.board:
-            print(i)
